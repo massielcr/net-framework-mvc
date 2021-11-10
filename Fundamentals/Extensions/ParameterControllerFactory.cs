5506da35274a6f9aee5bca9923edbb8fb640e593
Massiel <massiel.dev.ca@gmail.com>
2021-11-10 15:11:13 -0500

    clean up

--- Fundamentals/Extensions/ParameterControllerFactory.cs
+++ Fundamentals/Extensions/ParameterControllerFactory.cs
@@ -1,20 +1,16 @@
 ﻿using Fundamentals.Controllers;
 using Fundamentals.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.SessionState;
 
-namespace Fundamentals
+namespace Fundamentals.Extensions
 {
     public class ParameterControllerFactory : IControllerFactory
     {
         public IController CreateController(RequestContext requestContext, string controllerName)
         {
-            return (controllerName == "parameter") ? (IController)new ParameterController(new Logger()) : new HomeController();
+            return controllerName == "parameter" ? (IController)new ParameterController(new Logger()) : new HomeController();
         }
 
         public SessionStateBehavior GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
@@ -24,7 +20,7 @@ namespace Fundamentals
 
         public void ReleaseController(IController controller)
         {
-            
+
         }
     }
 }
--- Fundamentals/Global.asax.cs
+++ Fundamentals/Global.asax.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Fundamentals.Extensions;
 using System.Diagnostics;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
