83cd03d50af84856f58a34056e63ea27aa60a68c
Massiel <massiel.dev.ca@gmail.com>
2021-11-10 20:36:50 -0500

    added custom handler

--- Fundamentals/App_Start/RouteConfig.cs
+++ Fundamentals/App_Start/RouteConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fundamentals.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,22 @@ namespace Fundamentals
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // register route for custome hadler
+            routes.Add("Book", new Route("Book", new BookRouteHandler()));
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+        }
+    }
+
+    internal class BookRouteHandler : IRouteHandler
+    {
+        public IHttpHandler GetHttpHandler(RequestContext requestContext)
+        {
+            return new BookHandler();
         }
     }
 }
